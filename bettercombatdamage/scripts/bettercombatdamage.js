14b70892caf98e85aedf39b3c678e008dbe28304
Diogo Regateiro <diogoregateiro@ua.pt>
2023-01-25 10:37:23 +0000

    Refactor the logic so it works better.

--- bettercombatdamage/scripts/bettercombatdamage.js
+++ bettercombatdamage/scripts/bettercombatdamage.js
@@ -1,15 +1,74 @@
-// Returns whether an item makes an attack roll
-export function displayScrollingText(actor, diff, color) {
-	if ( !actor ) return;
-	const tokens = actor.getActiveTokens(true);
-	for ( const t of tokens ) {
-		canvas.interface.createScrollingText(t.center, diff.signedString(), {
-			anchor: CONST.TEXT_ANCHOR_POINTS.TOP,
-			fontSize: 48, // Range between [16, 48]
-			fill: color,
-			stroke: 0x000000,
-			strokeThickness: 4,
-			jitter: 0.25
-		});
+import { BCD_FIELDS, ActorUtils } from "./utils.js";
+import { BCDSettings } from "./settings.js"
+
+Hooks.on("updateActor", (actor, update, opts) => {
+	ActorUtils.updateActor(actor);
+});
+
+// Attach to actor updates
+Hooks.on("preUpdateActor", (actor, update, opts) => {
+	// If a supported value was updated, process the change
+	const updatedFields = ActorUtils.getUpdatedField(update);
+	for (let field of updatedFields) {
+		let diff = null, color = null;
+		switch(field) {
+			case BCD_FIELDS.HP:
+				// Calculate the change in HP
+				if (typeof opts.dhp !== 'undefined') {
+					diff = opts.dhp;
+				} else {
+					diff = update.system.attributes.hp.value - actor.system.attributes.hp.value;
+				}
+
+				// Check if Fortitude Points are on and whether the HP change would kill the actor
+				let fortitudeDiff = 0, hpDiff = diff;
+				if(BCDSettings.fortitudePointsEnabled && actor.system.resources?.legres?.value > 0 && actor.system.attributes.hp.value + diff <= 0) {
+					hpDiff = 1 - actor.system.attributes.hp.value;
+					fortitudeDiff = (diff - hpDiff);
+
+					// If the difference is such that completely exhausts the fortitude points, add the rest to the hpDiff
+					if (Math.abs(fortitudeDiff) > actor.system.resources.legres.value) {
+						fortitudeDiff = -actor.system.resources.legres.value;
+						hpDiff = diff - fortitudeDiff;
+					}
+
+					// Update the actor legendary resistance value
+					ActorUtils.addActorUpdate(actor, BCD_FIELDS.LEGENDARY_RESISTANCE, Math.max(actor.system.resources.legres.value + fortitudeDiff, 0));
+					ActorUtils.addActorUpdate(actor, BCD_FIELDS.HP, Math.max(actor.system.attributes.hp.value + hpDiff, 0));
+					if(BCDSettings.scrollTextEnabled && BCDSettings.legendaryResistanceEnabled) {
+						ActorUtils.addScrollingText(actor, fortitudeDiff, BCDSettings.legendaryResistanceColor);
+					}
+				}
+
+				// Display the hit points scrolling text
+				if(BCDSettings.scrollTextEnabled && BCDSettings.hitPointsEnabled) {
+					color = hpDiff <= 0 ? BCDSettings.hitPointsDamageColor : BCDSettings.hitPointsHealingColor;
+					ActorUtils.addScrollingText(actor, hpDiff, color);
+				}
+
+				ActorUtils.renderScrollingText(actor);
+				break;
+			case BCD_FIELDS.LEGENDARY_RESISTANCE:
+				// Calculate the legendary resistance difference	
+				diff = update.system.resources.legres.value - actor.system.resources.legres.value;
+
+				// Display the scrolling text
+				if(BCDSettings.scrollTextEnabled && BCDSettings.legendaryResistanceEnabled) {
+					ActorUtils.addScrollingText(actor, diff, BCDSettings.legendaryResistanceColor);
+				}
+
+				ActorUtils.renderScrollingText(actor);
+				break;
+			default:
+				// Do nothing if a non supported field is updated
+				return;
+		}
 	}
-}
+});
+
+Hooks.once("init", () => {
+	// Init the settings
+	BCDSettings.init();
+	// Disable the normal scrolling text
+	libWrapper.register("bettercombatdamage", "CONFIG.Actor.documentClass.prototype._displayScrollingDamage", (_) => {}, "OVERRIDE", {chain: true});
+});
--- bettercombatdamage/scripts/utils.js
+++ bettercombatdamage/scripts/utils.js
@@ -1,21 +1,91 @@
 // Supported fields
 export const BCD_FIELDS = {
-    LEGENDARY_RESISTANCE: "legres",
-    HP: "hp",
+    LEGENDARY_RESISTANCE: "system.resources.legres.value",
+    HP: "system.attributes.hp.value",
     NONE: ""
 }
 
-// Determines the updated field
-export function getUpdatedField(update) {
-    let fields = [];
+export class ActorUtils {
+	static ACTOR_UPDATES = {};
+    static ACTOR_UPDATING = {};
+    static TEXT_SCROLL = {};
 
-    if (typeof update.system?.attributes?.hp?.value !== 'undefined') {
-        fields.push(BCD_FIELDS.HP);
+	static addActorUpdate(actor, key, value) {
+		if (typeof ActorUtils.ACTOR_UPDATES[actor._id] === 'undefined') {
+			ActorUtils.ACTOR_UPDATES[actor._id] = {};
+		}
+
+        ActorUtils.ACTOR_UPDATES[actor._id][key] = value;
+	}
+
+	static updateActor(actor) {
+        if (ActorUtils.ACTOR_UPDATING[actor._id]) {
+            ActorUtils.ACTOR_UPDATING[actor._id] = false;
+        }
+        
+		// Update only if there is something to update
+		if(typeof ActorUtils.ACTOR_UPDATES[actor._id] === 'undefined' || Object.keys(ActorUtils.ACTOR_UPDATES[actor._id]).length === 0) {
+			return;
+		}
+
+		actor.update(ActorUtils.ACTOR_UPDATES[actor._id])
+		ActorUtils.ACTOR_UPDATES[actor._id] = {};
+        ActorUtils.ACTOR_UPDATING[actor._id] = true;
+	}
+
+    // Determines the updated field
+    static getUpdatedField(update) {
+        let fields = [];
+
+        if (typeof update.system?.attributes?.hp?.value !== 'undefined') {
+            fields.push(BCD_FIELDS.HP);
+        }
+
+        if(typeof update.system?.resources?.legres?.value !== 'undefined') {
+            fields.push(BCD_FIELDS.LEGENDARY_RESISTANCE);
+        } 
+
+        return fields;
     }
 
-    if(typeof update.system?.resources?.legres?.value !== 'undefined') {
-        fields.push(BCD_FIELDS.LEGENDARY_RESISTANCE);
-    } 
+    // Returns whether an item makes an attack roll
+    static addScrollingText(actor, diff, color) {
+        if (!actor || ActorUtils.ACTOR_UPDATING[actor._id] || diff === 0) return;
 
-    return fields;
+        if (typeof ActorUtils.TEXT_SCROLL[actor._id] === 'undefined') {
+            ActorUtils.TEXT_SCROLL[actor._id] = [];
+        }
+        
+        ActorUtils.TEXT_SCROLL[actor._id].push({diff, color});
+    }
+
+    static renderScrollingText(actor) {
+        if (typeof ActorUtils.TEXT_SCROLL[actor._id] === 'undefined') {
+            return;
+        }
+
+        for (let i = 0; i < ActorUtils.TEXT_SCROLL[actor._id].length; i++) {
+            const diff = ActorUtils.TEXT_SCROLL[actor._id][i].diff;
+            const color = ActorUtils.TEXT_SCROLL[actor._id][i].color;
+            setTimeout(() => ActorUtils._displayScrollingText(actor, diff, color), 750 * i);
+        }
+
+        ActorUtils.TEXT_SCROLL[actor._id] = [];
+    }
+
+    // Returns whether an item makes an attack roll
+    static _displayScrollingText(actor, diff, color) {
+        if ( !actor ) return;
+        const tokens = actor.getActiveTokens(true);
+        for ( const t of tokens ) {
+            canvas.interface.createScrollingText(t.center, diff.signedString(), {
+                anchor: CONST.TEXT_ANCHOR_POINTS.TOP,
+                fontSize: 48, // Range between [16, 48]
+                fill: color,
+                stroke: 0x000000,
+                strokeThickness: 4,
+                jitter: 0.25
+            });
+        }
+    }
 }
