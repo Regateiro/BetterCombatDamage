3e60fa33dd3acd016db4207660e5125ef6db8e90
Diogo Regateiro <diogoregateiro@ua.pt>
2023-02-12 22:16:35 +0000

    Try to fix issue with non PC sheets not being able to roll.

--- bettercombatdamage/scripts/utils.js
+++ bettercombatdamage/scripts/utils.js
@@ -88,9 +88,9 @@ export class ActorUtils {
 };
 
 export async function _rollAttack(wrapper, config) {
-    if(this.parent.flags.dnd5e.bladeMastery && ["dagger", "longsword", "shortsword", "scimitar", "rapier", "greatsword"].includes(this.system.baseItem)) {
+    if(this.parent?.flags?.dnd5e?.bladeMastery && ["dagger", "longsword", "shortsword", "scimitar", "rapier", "greatsword"].includes(this.system.baseItem)) {
         config['elvenAccuracy'] = true;
-    } else if(this.parent.flags.dnd5e.greaterRage) {
+    } else if(this.parent?.flags?.dnd5e?.greaterRage) {
         config['elvenAccuracy'] = true;
     }
     return await wrapper.call(this, config);
